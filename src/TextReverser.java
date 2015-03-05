d870f07a2bc6260ae3f2c2930795dc1ab0066500
mmelle <aojmathiasmel@gmail.com>
2015-03-05 23:27:28 +0100

    0.2

--- src/TextReverser.java
+++ src/TextReverser.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
-/**
- * Created by Mathias on 05.03.2015.
- */
 public class TextReverser {
     static Scanner input = new Scanner(System.in);
+    private static int counter;
 
     public static void reverse(String text){
+
         if (text.length() != 0){
             String temp = text;
             System.out.print(text.charAt(text.length()-1));
@@ -16,9 +15,23 @@ public class TextReverser {
         }
     }
 
+    public static void countSymbols(String text) {
+            for(int i = 0; i < text.length(); i++){
+                if(text.charAt(i) != ' '){
+                counter++;
+                }
+            }
+        }
+
+
+
     public static void main(String[] args){
         System.out.println("Vennligst skriv inn en streng: ");
-        String text = input.next();
-        reverse(text);
+            String text = input.nextLine();
+            reverse(text);
+            countSymbols(text);
+
+        System.out.println("Number of symbols: " + counter);
     }
+
 }
