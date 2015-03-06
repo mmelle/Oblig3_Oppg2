2d9cf48f0503721b8f7d7a947f1653f0fa3b84e8
mmelle <aojmathiasmel@gmail.com>
2015-03-06 10:05:10 +0100

    0.3

--- src/TextReverser.java
+++ src/TextReverser.java
@@ -4,15 +4,20 @@ public class TextReverser {
     static Scanner input = new Scanner(System.in);
     private static int counter;
 
-    public static void reverse(String text){
+    public static void reverse(String text, int last){
 
-        if (text.length() != 0){
-            String temp = text;
-            System.out.print(text.charAt(text.length()-1));
-            reverse(text.substring(0, text.length() - 1));
+        if (last == 0) {
+            System.out.println(text.charAt(last));
         } else {
-         System.out.println();
+            System.out.print(text.charAt(last));
+            reverse(text, last-1);
         }
+    }
+
+
+    public static void reverseInitiate(String text){
+        int last = text.length()-1;
+        reverse(text, last);
     }
 
     public static void countSymbols(String text) {
@@ -28,7 +33,7 @@ public class TextReverser {
     public static void main(String[] args){
         System.out.println("Vennligst skriv inn en streng: ");
             String text = input.nextLine();
-            reverse(text);
+            reverseInitiate(text);
             countSymbols(text);
 
         System.out.println("Number of symbols: " + counter);
